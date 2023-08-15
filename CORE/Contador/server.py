63a97d3e66866313f4c9d08b6db16e4d6ba1d309
Gessica <adarogessica@gmail.com>
2023-08-15 16:49:57 -0400

    ejercicios Flask

--- CORE/Contador/server.py
+++ CORE/Contador/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
-app.secret_key = 'miclavesecreta'
+app.secret_key = 'miclavesecreta-'
 
 @app.route('/')
 def index():
